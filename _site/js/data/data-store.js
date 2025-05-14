fc31483979e613044a5e8774e90995353cc12a47
JaZeeGH <jason.hajdics@protonmail.com>
2025-05-14 22:23:50 +0100

    modified:   _site/js/data/data-store.js 	modified:   js/data/data-store.js

--- _site/js/data/data-store.js
+++ _site/js/data/data-store.js
@@ -1,38 +1,44 @@
 const dataStore = {
   data: [
-        {
-          city: "amsterdam",
-          name: "Amsterdam",
-          imageUrl: "https://unsplash.com/photos/body-of-water-under-white-sky-QRtym77B6xk"
-        },
-        {
-          city: "berlin",
-          name: "Berlin",
-          imageUrl: "https://unsplash.com/photos/city-buildings-near-body-of-water-during-daytime-1uWanmgkd5g"
-        },
-        {
-          city: "copenhagen",
-          name: "Copenhagen",
-          imageUrl: "https://unsplash.com/photos/assorted-color-buildings-near-red-boat-docked-on-port-during-daytime-I17LBNJq9q8"
-        },
-        {
-          city: "cork",
-          name: "Cork",
-          imageUrl: "https://unsplash.com/photos/a-large-city-with-a-clock-tower-vV-wXd-V2CQ"
-        },
-        {
-          city: "paris",
-          name: "Paris",
-          imageUrl: "https://unsplash.com/photos/vehicles-travelling-on-road-surrounded-by-buildings-during-daytime-DXuxHw3S5ak"
-        },
-        {
-          city: "waterford",
-          name: "Waterford",
-          imageUrl: "https://www.pexels.com/photo/aerial-view-of-tramore-s-scenic-playground-31575501/"
-        }
-      ],
-
-    list(){
-        return this.data
+    {
+      city: "amsterdam",
+      name: "Amsterdam",
     },
+    {
+      city: "berlin",
+      name: "Berlin",
+    },
+    {
+      city: "copenhagen",
+      name: "Copenhagen",
+    },
+    {
+      city: "cork",
+      name: "Cork",
+    },
+    {
+      city: "new_york",
+      name: "New York",
+    },
+    {
+      city: "paris",
+      name: "Paris",
+    },
+    {
+      city: "san_francisco",
+      name: "San Francisco",
+    },
+    {
+      city: "tromso",
+      name: "Tromso",
+    },
+    {
+      city: "waterford",
+      name: "Waterford",
+    }
+  ],
+
+  list() {
+    return this.data
+  },
 }
--- js/data/data-store.js
+++ js/data/data-store.js
@@ -1,35 +1,44 @@
 const dataStore = {
   data: [
-        {
-          city: "amsterdam",
-          name: "Amsterdam",
-        },
-        {
-          city: "berlin",
-          name: "Berlin",
-        }
-        {
-          city: "copenhagen",
-          name: "Copenhagen",
-        },
-        {
-          city: "cork",
-          name: "Cork",
-          imageUrl: "https://unsplash.com/photos/a-large-city-with-a-clock-tower-vV-wXd-V2CQ"
-        },
-        {
-          city: "paris",
-          name: "Paris",
-          imageUrl: "https://unsplash.com/photos/vehicles-travelling-on-road-surrounded-by-buildings-during-daytime-DXuxHw3S5ak"
-        },
-        {
-          city: "waterford",
-          name: "Waterford",
-          imageUrl: "https://www.pexels.com/photo/aerial-view-of-tramore-s-scenic-playground-31575501/"
-        }
-      ],
-
-    list(){
-        return this.data
+    {
+      city: "amsterdam",
+      name: "Amsterdam",
     },
+    {
+      city: "berlin",
+      name: "Berlin",
+    },
+    {
+      city: "copenhagen",
+      name: "Copenhagen",
+    },
+    {
+      city: "cork",
+      name: "Cork",
+    },
+    {
+      city: "new_york",
+      name: "New York",
+    },
+    {
+      city: "paris",
+      name: "Paris",
+    },
+    {
+      city: "san_francisco",
+      name: "San Francisco",
+    },
+    {
+      city: "tromso",
+      name: "Tromso",
+    },
+    {
+      city: "waterford",
+      name: "Waterford",
+    }
+  ],
+
+  list() {
+    return this.data
+  },
 }
